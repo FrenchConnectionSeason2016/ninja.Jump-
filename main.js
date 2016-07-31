be5144dce3de1adf95bf7dd3f7bfc197e04c4d66
gyankov <g.yankov951@abv.bg>
2016-07-31 19:56:58 +0300

    Jump properly made

--- main.js
+++ main.js
@@ -1,160 +1,190 @@
 
-			
-	var ninja,
-		ninjaRun,
-		ninjaJump,
-		canvas,
-		frameIndex = 0;					
 
-	function gameLoop () {
-	
-	  window.requestAnimationFrame(gameLoop);
+var ninja,
+	ninjaRun,
+	ninjaJump,
+	canvas,
+	frameIndex = 0;
 
-	  ninja.update();
-	  ninja.render();
-	}
-	
-	function sprite (options) {
-	
-		var that = {},			
-			tickCount = 0,
-			ticksPerFrame = options.ticksPerFrame || 0 ;
-			
-			//numberOfFrames = options.numberOfFrames || 1;
+
+function gameLoop() {
+
+	window.requestAnimationFrame(gameLoop);
+
+	ninja.update();
+	ninja.updateJumpIndex();
+	ninja.render();
+}
+
+function sprite(options) {
+
+	var that = {},
+		tickCount = 0,
+		ticksPerFrame = options.ticksPerFrame || 0,
+		jumpIndex = 0;
+
+	//numberOfFrames = options.numberOfFrames || 1;
 
 		  var altitude = 336;
+
+
+	that.context = options.context;
+	that.width = options.width;
+	that.height = options.height;
+	that.image = options.image;
+	that.numberOfFrames = options.numberOfFrames || 1;
+	that.jumping = options.jumping;
+
+	//UPDATE
+
+	that.update = function () {
+
+		
+
+		tickCount += 1;
+
+		if (tickCount > ticksPerFrame) {
+
+			tickCount = 0;
+
+			if (frameIndex < that.numberOfFrames - 1) {
+
+				frameIndex += 1;
+			} else {
+				frameIndex = 0;
+			}
+		}
+	};
+
+
+	that.updateJumpIndex = function () {
+
+		if (that.jumping) {
 			
+			if ( jumpIndex < 17) {
+				altitude -= 6;
+			} else {
+				altitude += 6;
+			}
+
+			jumpIndex+=1;
+
+		} else {
+			altitude = 336;
+		}
 		
-		that.context = options.context;	
-		that.width = options.width;
-		that.height = options.height;
-		that.image = options.image;
-		that.numberOfFrames= options.numberOfFrames || 1;
-		that.jumping = options.jumping;
 
-		//UPDATE
-		
-		that.update = function () {
+	}
 
-            tickCount += 1;
 
-            if (tickCount > ticksPerFrame) {
+	//RENDER
+	that.render = function () {
 
-				tickCount = 0;
-			
-                if (frameIndex < that.numberOfFrames -1) {	
-                 
-                    frameIndex += 1;
-                } else {
-                    frameIndex = 0;
-                }
-            }
-        };
-		
-		//RENDER
-		that.render = function () {
-		
-		 
 
-		  that.context.clearRect(0, 0, 600, 400);
-		  
 
-		  if(that.jumping && frameIndex < 4){
-			  altitude-=6;
-		  }else if(that.jumping){
-			  altitude+=6;
-		  }else{
-			  altitude=336;
-		  }
+		that.context.clearRect(0, 0, 600, 400);	
 
-		 
 	
+		// if (that.jumping) {
+		// 		console.log(jumpIndex);	
+
+
+		// 	if ( jumpIndex < 17) {
+		// 		altitude -= 6;
+		// 	} else {
+		// 		altitude += 6;
+		// 	}
+
+		// 	jumpIndex+=1;
+
+		// } else {
+		// 	altitude = 336;
+		// }
+		
 		    that.context.drawImage(
-		    that.image,
-		    frameIndex * that.width /that.numberOfFrames,
-		    0,
-		    that.width / that.numberOfFrames,
-		    that.height,
-		    270,
-		    altitude,
-		    that.width / that.numberOfFrames,
-		    that.height);
-         
-         	var ctx = canvas.getContext('2d');
-         	ctx.beginPath();
-         	ctx.moveTo(0,250);
-         	ctx.lineTo(600,250);
-         	ctx.stroke();
-         
+			that.image,
+			frameIndex * that.width / that.numberOfFrames,
+			0,
+			that.width / that.numberOfFrames,
+			that.height,
+			270,
+			altitude,
+			that.width / that.numberOfFrames,
+			that.height);
 
-			if(that.jumping && frameIndex===(that.numberOfFrames-1)){				
+		var ctx = canvas.getContext('2d');
+		ctx.beginPath();
+		ctx.moveTo(0, 250);
+		ctx.lineTo(600, 250);
+		ctx.stroke();
 
-			  Run();
-			  frameIndex=0;
-		  }
-		};
 
-		 
-		
-		return that;
+		if (that.jumping && frameIndex === (that.numberOfFrames - 1)) {
+
+			jumpIndex=0;	
+			Run();
+			frameIndex = 0;	
+		}
+	};
+
+
+
+	return that;
+}
+
+
+canvas = document.getElementById("gamePlace");
+canvas.width = 600;
+canvas.height = 400;
+
+
+ninjaRun = new Image();
+ninjaRun.src = "run.png";
+
+ninjaJump = new Image();
+ninjaJump.src = 'jump.png';
+
+
+
+ninja = sprite({
+	context: canvas.getContext("2d"),
+	width: 512,
+	height: 64,
+	image: ninjaRun,
+	numberOfFrames: 8,
+	ticksPerFrame: 4,
+	jumping: false,
+
+
+});
+
+
+function Jump(ev) {
+
+	console.log('tuk')
+	if (ev.keyCode == 32 && !ninja.jumping) {
+		ninja.jumping = true;
+		ninja.image = ninjaJump;
+		frameIndex = 0,
+			ticksPerFrame = 6;
 	}
-	
-	
-	canvas = document.getElementById("gamePlace");
-	canvas.width = 600;
-	canvas.height = 400;
-	
 
-	ninjaRun = new Image();	
-	ninjaRun.src = "run.png";
+}
 
-	ninjaJump= new Image();
-	ninjaJump.src= 'jump.png';
-	
-	
-	
-	ninja = sprite({
-		context: canvas.getContext("2d"),
-		width: 512,
-		height: 64,
-		image: ninjaRun,
-		numberOfFrames: 8,
-		ticksPerFrame: 4,
-		jumping: false,
+function Run() {
+
+	ninja.jumping = false;
+	ninja.image = ninjaRun,
+		ticksPerFrame = 4;
+}
 
 
-	});
-
-
-	function Jump(ev) {	
-
-		console.log('tuk')
-		if(ev.keyCode == 32 && !ninja.jumping){
-			ninja.jumping= true;
-    	    ninja.image=ninjaJump;
-			frameIndex=0,
-			ticksPerFrame=6;		 
-		}	
-
-	}
-
-	function Run() {
-
-			ninja.jumping= false;
-    	    ninja.image=ninjaRun,
-			ticksPerFrame=4;
-		
-					
-	}
-	
-	
 
 //var body = document.getElementsByTagName('body')[0];
 
 window.addEventListener('load', gameLoop, false);
 
-window.addEventListener('keyup', Jump, false );
-
+window.addEventListener('keyup', Jump, false);
 
 
 
